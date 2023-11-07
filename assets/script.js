cd5fcb3af4c664bb9b10a20829a0bbb046c0ef67
exsider11 <exsider1231@gmail.com>
2023-11-06 20:43:04 -0500

    added values for search bar text

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
-$( function() {
-    $( "#format" ).selectmenu();
+
+
+var searchButton = document.querySelector("#search-button")
+
+searchButton.addEventListener("click", function(event){
+    event.preventDefault();
+    var searchParameters = document.querySelector("#searchBar").innerHTML;
+    console.log(searchParameters)
 })
